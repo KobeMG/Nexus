82e508e10b1d6c69f707b740dc2d7c5315aaedc2
KobeMG <kobemoya@gmail.com>
2025-08-28 15:05:05 -0600

    Add project files.

---
+++ Proyecto Nexus/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿namespace Proyecto_Nexus.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Price { get; set; }
+
+        public int Stock { get; set; }
+
+        public string ImageUrl { get; set; }
+    }
+}
